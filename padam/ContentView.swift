8b46a626e841c8aced7deae07ac3954a9e8b5fed
dohangusto <dohangusto.samu@gmail.com>
2026-09-07 23:12:14 +0700

    Initial Commit

---
+++ padam/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  padam
+//
+//  Created by Ivandohan Samuel Siregar on 07/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ padam/padamApp.swift
@@ -0,0 +1,17 @@
+//
+//  padamApp.swift
+//  padam
+//
+//  Created by Ivandohan Samuel Siregar on 07/09/26.
+//
+
+import SwiftUI
+
+@main
+struct padamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
